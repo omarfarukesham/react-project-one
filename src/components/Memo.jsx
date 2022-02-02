1695b9090829a3c00f78ea09fb4dd16ae0eef28c
Omar Faruk <omarfarukesham@gmail.com>
2022-02-02 18:27:45 +0600

    first-react-commit

---
+++ src/components/Memo.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import memesData from "../memesData.js"
+
+export default function Memo() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+       
+    }
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div>
+                <h1>Hello </h1>
+                <img src="" width="200px" alt="" />
+            </div>
+        </main>
+    )
+}
